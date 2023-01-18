5a03a172ef2cb860617f2eaa876bb21eca1d0fd0
DhanashriShirke <122086072+DhanashriShirke@users.noreply.github.com>
2023-01-18 13:53:28 +0530

    Add files via upload

---
+++ HollowBox.java
@@ -0,0 +1,26 @@
+class HollowBox
+{
+	public static void main(String[] args) 
+{
+int rows=5,columns=5;
+for(int i=1;i<=5;i++)
+{
+for(int j=1;j<=5;j++)
+{
+if(i==1||i==5||j==1||j==5);
+{
+System.out.print("*");
+}
+else 
+{
+System.out.println(" ");
+}
+j++;
+}
+System.out.println();
+i++;
+print_rectangle(rows,coloumns);
+}
+}
+}
+
